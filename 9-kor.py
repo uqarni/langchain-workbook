b90745ea23cd325dccb14f7a3a93a0c4b851ba77
uqarni <108438834+uqarni@users.noreply.github.com>
2023-05-28 01:27:28 +0000

    more kor

--- 9-kor.py
+++ 9-kor.py
@@ -43,3 +43,92 @@ person_schema = Object(
         ]
     )
     
+chain = create_extraction_chain(llm, person_schema)
+
+text = "My name is Bobby. My sister's name is Rachel. My brother's name is Joe. My dog's name is Spot."
+#output = chain.predict_and_parse(text = (text))["data"]
+#print(output)
+
+#output = chain.predict_and_parse(text = "The dog went to the park")["data"]
+#print(output)
+
+plant_schema = Object(
+    id="plant",
+    description = "Information about a plant",
+
+    attributes = [
+        Text(
+            id = "plant_type",
+            description = "The common name of the plant",
+        ),
+        Text(
+            id = "color",
+            description = "The color of the plant",
+        ),
+        Number(
+            id = "rating",
+            description = "The rating of the plant",
+        )
+    ],
+    examples = [
+        (
+            "Roses are red, lilies are white and an 8 out of 10",
+            [
+            {"plant_type": "roses", "color": "red"},
+            {"plant_type": "lilies", "color": "white", "rating": 8},
+            ],
+        )
+    ]
+)
+
+text = "Plan trees are brown with a 6 rating.. Sequioa trees are green."
+
+chain = create_extraction_chain(llm, plant_schema)
+#output = chain.predict_and_parse(text = text)["data"]
+#print(output)
+
+parts = Object(
+    id = "parts",
+    description = "Parts of a car",
+    attributes = [
+        Text(id = "part", description = "The name of the part of a car")
+    ],
+    examples=[
+        ("the jeep has an wheels and windows", 
+            [
+                {"part": "wheels"},    
+                {"part": "windows"}
+            ],
+        )
+    ]
+)
+
+cars_schema = Object(
+    id = "car",
+    description = "Information about a car",
+    examples = [
+        (
+            "the bmw is red and has an engine and steering wheel",
+            [
+                {"type": "BMW", "color": "red", "parts": ["engine", "steering wheel"]}
+            ],
+        )
+    ],
+    attributes=[
+        Text(
+            id = "type",
+            description = "The make or brand of car",
+        ),
+        Text(
+            id = "color",
+            description = "The color of the car",
+        ),
+        parts
+    ]
+)
+
+text = "The blue Mercedes Benze has a rearview mirror, roof, and windwhield. Inside sits a girl wearing pants. On those pants she's wearing a huge Texan leather belt."
+
+chain = create_extraction_chain(llm, cars_schema, encoder_or_encoder_class = "json")
+output = chain.predict_and_parse(text = text)["data"]
+print(output)
